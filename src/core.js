e22cf228bb9059e772551d781955c615e0da19da
joshhartigan <joshhartigan99@gmail.com>
2014-11-28 07:27:27 +0000

    some improved commenting

--- src/core.js
+++ src/core.js
@@ -20,7 +20,7 @@ var constants = {
 }
 
 /**
- * initialise:
+ * initialise():
  *   set up the constants for vws
  *   and get things ready for
  *   interaction.
@@ -46,8 +46,8 @@ var menuBar = {
   /*
   'example': {
     position: {
-      start: [x-coord of where the button starts],
-      end: [x-coord of where the button ends]
+      start: [x-coord of where the button starts - default 0],
+      end: [x-coord of where the button ends - default 0]
     },
     call: [whichever function 'example' should call]
     active: true // whether or not 'example' can be selected
@@ -67,6 +67,12 @@ var menuBar = {
   }
 }
 
+/**
+ * drawMenuBar():
+ *   put the menu bar on the canvas
+ *   and fill it with text for each
+ *   item.
+ */
 function drawMenuBar() {
   graphics.fillStyle = constants.menuColor
   graphics.fillRect(0, 0, WIDTH, constants.menuHeight)
@@ -97,6 +103,12 @@ function clickListener() {
   })
 }
 
+/**
+ * menuClickListener():
+ *   carry out events after clicking
+ *   on the menu bar, depending on
+ *   the x coord of the cursor.
+ */
 function menuClickListener(x) {
   for (item in menuBar) {
     if (x >= menuBar[item].position.start && x <= menuBar[item].position.end) {
